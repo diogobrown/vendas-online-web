e3ffdc73df312717bd194e2f5a08a1a333b931d3
Diogo Brown <diogo_brown95@hotmail.com>
2025-11-08 17:22:53 -0300

    feat: insert antd and styled-componentes

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import styled from 'styled-components'
+import { Button } from 'antd'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <StyledLink isBlue={count >4} className='App'>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -18,9 +20,9 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <Button onClick={() => setCount((count) => count + 1)}>
           count is {count}
-        </button>
+        </Button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
@@ -28,8 +30,16 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+    </StyledLink>
   )
 }
 
-export default App
+interface PStyledLinnk{
+  isBlue?: boolean;
+}
+
+const StyledLink = styled.div<PStyledLinnk>`
+  color: ${(props) => (props.isBlue ? 'blue' : 'pink')};
+`;
+
+export default App;
