0eb5b3830d237033b15e38182ad172e1e6f552c9
Diogo Brown <diogo_brown95@hotmail.com>
2025-11-12 15:56:55 -0300

    feat: inset state in login and style input password

--- src/shared/inputs/input/input.tsx
+++ src/shared/inputs/input/input.tsx
@@ -1,19 +1,36 @@
-import {Input as InputAntd, type InputProps as InputPropsAntd} from 'antd';
-import { BoxInput, TitleInput } from './input.styles';
+import { useState } from "react";
+import { Input as InputAntd, type InputProps as InputPropsAntd } from "antd";
+import { EyeInvisibleOutlined, EyeTwoTone } from "@ant-design/icons";
+import { BoxInput, TitleInput } from "./input.styles";
 
-
-interface InputProps extends InputPropsAntd{
-    title?:string;
+interface InputProps extends InputPropsAntd {
+  title?: string;
+  margin?: string;
 }
 
-const Input = ({title, ...props}: InputProps) => {
-    return (
-        <BoxInput>
-            <TitleInput>{title}</TitleInput>
-            <InputAntd {...props}/>
-        </BoxInput>
-        
-    )
+const Input = ({ title, margin, type, ...props }: InputProps) => {
+  const [passwordVisible, setPasswordVisible] = useState(false);
+
+  return (
+    <BoxInput style={{ margin }}>
+      {title && <TitleInput>{title}</TitleInput>}
+
+      {type === "password" ? (
+        <InputAntd.Password
+          {...props}
+          iconRender={(visible) =>
+            visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />
+          }
+          visibilityToggle={{
+            visible: passwordVisible,
+            onVisibleChange: setPasswordVisible,
+          }}
+        />
+      ) : (
+        <InputAntd {...props} type={type} />
+      )}
+    </BoxInput>
+  );
 };
 
 export default Input;
